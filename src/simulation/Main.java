fcdb66f246a2d07aecf64d746a19468aec64727b
Samuel Svensäter <peterodd@gmail.com>
2014-04-22 11:55:59 +0200

    Added some stuff to main

---
+++ src/simulation/Main.java
@@ -0,0 +1,14 @@
+package simulation;
+
+import utilities.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Globals.height = 65;
+		Globals.width = 65;
+		Globals.heightmap = new float[Globals.width][Globals.height];
+
+	}
+
+}
