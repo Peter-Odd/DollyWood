c937a605d93396d1e6f507fdce423afbae327e66
Samuel Svensäter <peterodd@gmail.com>
2014-04-22 12:18:36 +0200

    Added simple graphics

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -1,14 +1,18 @@
 package simulation;
 
 import utilities.*;
-
+import graphics.*;
 public class Main {
 
 	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public Main() {
 		Globals.height = 65;
 		Globals.width = 65;
 		Globals.heightmap = new float[Globals.width][Globals.height];
-
+		new Graphics2D();
 	}
 
 }
