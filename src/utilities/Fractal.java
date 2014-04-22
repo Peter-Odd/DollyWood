36ef5e6059e88f65e90e324d0c61edf5a22dd6eb
Jimmy <jimmy.helmersson.6800@student.uu.se>
2014-04-22 12:22:19 +0200

    Added some methods for Fractal

--- src/utilities/Fractal.java
+++ src/utilities/Fractal.java
@@ -1,5 +1,24 @@
 package utilities;
 
-public class Fractal {
+import java.util.Random;
 
+public class Fractal {
+	
+	
+	public static float getRandom(){
+		Random r = new Random();
+		return r.nextFloat();
+	}
+	
+	public static float[][] generateFractal(float[][] map, float min, float max){
+		
+		return map;
+	}
+	
+	private static float[][] intitateCorners(float[][] map){
+		int width = map.length;
+		int height = map[0].length;
+		return map;
+	}
 }
+
