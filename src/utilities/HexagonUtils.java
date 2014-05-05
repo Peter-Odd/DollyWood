3603403bfaccb28582a294d4231a3d7e3faaece9
Jimmy <jimmy.helmersson.6800@student.uu.se>
2014-05-05 12:28:06 +0200

    Merge

--- src/utilities/HexagonUtils.java
+++ src/utilities/HexagonUtils.java
@@ -1,36 +1,64 @@
 package utilities;
+
+import java.util.ArrayList;
+
 /**
  * UtilityObject that will mostly help with converting a square map to a hexagonal map.
  * @author OSM Group 5 - DollyWood project
  * @version 1.0
  */
 public class HexagonUtils {
-
 	/**
 	 * Calculates neighboring points
 	 * @param x X-centerPoint
 	 * @param y Y-centerPoint
 	 * @return Returns a two-dimensional array containing all points that touch (x,y) in a hexagonal grid
 	 */
-	public static int[][] neighborTiles(int x, int y) {
-		int[][] tiles = new int[6][2];
+	public static ArrayList<int[]> neighborTiles(int x, int y, boolean includeCenter) {
+		ArrayList<int[]> tiles = new ArrayList<>();
+		int[] tile = new int[2];
 		//Y-Tiles
-		tiles[0][0] = x;
-		tiles[0][1] = y+1;
-		tiles[1][0] = x;
-		tiles[1][1] = y-1;
+		tile[0] = x;
+		tile[1] = y+1;
+		tiles.add(tile);
+		tile = new int[2];
+		tile[0] = x;
+		tile[1] = y-1;
+		tiles.add(tile);
 		
 		//X-Tiles (offset)
-		tiles[2][0] = x+1;
-		tiles[2][1] = y+(x%2);
-		tiles[3][0] = x-1;
-		tiles[3][1] = y+(x%2);
+		tile = new int[2];
+		tile[0] = x+1;
+		tile[1] = y+(x%2);
+		tiles.add(tile);
+		tile = new int[2];
+		tile[0] = x-1;
+		tile[1] = y+(x%2);
+		tiles.add(tile);
 		
 		//X-Tiles (no-offset)
-		tiles[4][0] = x+1;
-		tiles[4][1] = y+(x%2)-1;
-		tiles[5][0] = x-1;
-		tiles[5][1] = y+(x%2)-1;
+		tile = new int[2];
+		tile[0] = x+1;
+		tile[1] = y+(x%2)-1;
+		tiles.add(tile);
+		tile = new int[2];
+		tile[0] = x-1;
+		tile[1] = y+(x%2)-1;
+		tiles.add(tile);
+		if(includeCenter){
+			tile = new int[2];
+			tile[0] = x;
+			tile[1] = y;
+			tiles.add(tile);
+		}
+		for(int[] tileIterator : tiles){
+			tileIterator[0] %= Globals.width;
+			tileIterator[1] %= Globals.height;
+			if(tileIterator[0] < 0)
+				tileIterator[0] = Globals.width - tileIterator[0]-2;
+			if(tileIterator[1] < 0)
+				tileIterator[1] = Globals.height - tileIterator[1]-2;
+		}
 		return tiles;
 	}
 
