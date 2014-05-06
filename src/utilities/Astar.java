2f0354d892197311aee829a1828d4dd3dafca7c2
Samuel Svensäter <peterodd@gmail.com>
2014-05-06 14:47:29 +0200

    Working A* algorithm.

--- src/utilities/Astar.java
+++ src/utilities/Astar.java
@@ -1,12 +1,9 @@
 package utilities;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
-
 import simulation.Race;
 
 
@@ -17,12 +14,8 @@ import simulation.Race;
  */
 
 public class Astar {
-
 	static LinkedList<Node> openList = new LinkedList<>(); //contains nodes to visit, possibly sort after TotalCost
 	static ArrayList<Node> closedList = new ArrayList<>(); //contains visited nodes
-	//	static ArrayList<Node> obstacleList = new ArrayList<>(); //contains unwalkable objects
-
-	//	static int count = 0;
 	final static int VERYHIGHVALUE = 999999;
 
 
@@ -35,7 +28,6 @@ public class Astar {
 	 * @return Distance from start node to goal node
 	 */
 	private static int calculateDistanceToGoal(int startX, int startY, int goalX, int goalY) {
-		//	System.out.println("Distance: " + Math.abs((goalX-startX)+(goalY-startY)));
 		return ( Math.abs(goalX-startX) + Math.abs(goalY-startY) );
 	}
 
@@ -55,7 +47,6 @@ public class Astar {
 				returnMe = l;
 			}
 		}
-		//		System.out.println("Lowest total cost: " + returnMe.getTotalCost());
 		return returnMe;
 	}
 
@@ -64,25 +55,25 @@ public class Astar {
 	 * @param list
 	 * @return Stack with elements from head in list to just before null
 	 */
-	private static Stack<Node> tracePath(int position) {
+	private static Deque<Node> tracePath(int position) {
 		System.out.println("GOAL FOUND, EUREKA!");
+		//Stack<Node> resultStack = new Stack<>();
+		Deque<Node> resultStack = new ArrayDeque<>();
 
-		Stack<Node> resultStack = new Stack<>();
 		Node cursor = closedList.get(position);
 
 		while (cursor != null) {
-			System.out.println("(" + cursor.getX() + "," + cursor.getY() + ")." );	
-			resultStack.push(cursor);
+			resultStack.addFirst(cursor);
 			cursor = cursor.getParent();
 		}
 		return resultStack;
 	}	
 
 	/**
-	 * 
-	 * @param x
-	 * @param y
-	 * @return 
+	 * Checks if species on x and y-coordinate
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 * @return false if species on (x,y)-coordinate, true otherwise
 	 */
 	private static boolean noSpecies(int x, int y) {
 		boolean walkable = true;
@@ -95,14 +86,16 @@ public class Astar {
 		return walkable;
 	}
 
+	/**
+	 * Calculates movement cost depending on heightmap found in Globals.heightmap
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 * @return Cost for moving to (x,y)-coordinate
+	 */
 	private static int calculateMovementCost(int x, int y) {
-		// 	System.out.println("WORLD AT X, Y: " + x + ", " + y + ": " + (int) (AstarDriver.world[x][y]));
-		return (int) (AstarDriver.world[x][y]);
-		//return (int) (Globals.heightmap[x][y] / 20);
-
+		return (int) (AstarDriver.world[x][y]); //for test purpose only
+		//return (int) (Globals.heightmap[x][y] / 20); //use this when used in DollyWood
 	}
-
-
 
 	/**
 	 * Calculates path from (startX, startY) to (goalX, goalY) within hexagonal world found in Globals.
@@ -110,20 +103,20 @@ public class Astar {
 	 * @param startY Y-coordinate of start node
 	 * @param goalX X-coordinate of goal node
 	 * @param goalY Y-coordinate of goal node
-	 * @return List with elements representing shortest path from start to goal, null if goal not found.
+	 * @return Deque with elements representing shortest path from start to goal, null if goal not found.
 	 */
 
-	public static Stack<Node> calculatePath(int startX, int startY, int goalX, int goalY) {
+	public static Deque<Node> calculatePath(int startX, int startY, int goalX, int goalY) {
 		Node start = new Node(startX, startY, calculateDistanceToGoal(startX, startY, goalX, goalY), 0, null);
 		openList.add(start);
 		boolean goalFound = false;
 
+		//		USE BELOW TO TIME THE PATHFINDER
 		//		long time = System.currentTimeMillis();
 
 		do {
 			Node currentNode = findLowestTotalCost(openList);
 			openList.remove(currentNode);
-			//System.out.println("currentNode:" + currentNode.getX() + ":" + currentNode.getY());
 
 			if (currentNode.getX() == goalX && currentNode.getY() == goalY) {
 				//goal found
@@ -136,13 +129,13 @@ public class Astar {
 			for (int[] neighbor : neighbors) {
 
 				if (noSpecies(neighbor[0], neighbor[1])) { //if no species next to neighbor continue, else skip code below
-					
+
 					Node newNode = new Node(neighbor[0], neighbor[1], calculateDistanceToGoal(neighbor[0], neighbor[1], goalX, goalY), currentNode.getMovementCost() + calculateMovementCost(neighbor[0],  neighbor[1]) + 1, currentNode);
 
 					boolean existsInOpenList = false;
 
 					for (Node nodeInOpenList : openList) {
-						if (nodeInOpenList.getX() == newNode.getX() && nodeInOpenList.getY() == newNode.getY()) { //check X&Y-value n and newNode
+						if (nodeInOpenList.getX() == newNode.getX() && nodeInOpenList.getY() == newNode.getY()) { //check X&Y-value for n and newNode
 							if (nodeInOpenList.getMovementCost() < newNode.getMovementCost()) {
 								//node exists in openList, do not add it to the openList.
 								//nothing to do, exit foreach-loop
@@ -160,7 +153,6 @@ public class Astar {
 					}
 
 					if (!existsInOpenList) {
-						//System.out.println("Adding to openList:" + neighbor[0] + ":" + neighbor[1]);
 						openList.add(newNode);
 					}
 				}
@@ -173,151 +165,10 @@ public class Astar {
 			return null; 
 		}
 
+		//		USE BELOW TO TIME THE PATHFINDER
 		//		System.out.println("TIME TAKEN: " + (System.currentTimeMillis() - time));
 		//		System.out.println("OPEN LIST SIZE: " + openList.size() + ", ClosedList size: " + closedList.size());
 
 		return tracePath(closedList.size() - 1);
-
-
-
 	}
-
-	//	public static Stack<Node> calculatePath(int startX, int startY, int goalX, int goalY) {
-	//		boolean goalFound = false;
-	//		Node start = new Node(startX, startY, calculateDistanceToGoal(startX, startY, goalX, goalY), 0, null);
-	//		openList.add(start);
-	//
-	//		do {
-	//			Node currentNode = findLowestTotalCost(openList);
-	//			openList.remove(currentNode);
-	//
-	//			if (currentNode.getX() == goalX && currentNode.getY() == goalY) {
-	//				closedList.add(currentNode);
-	//				goalFound = true;
-	//				break;
-	//			}
-	//
-	//			ArrayList<int[]> neighbors = HexagonUtils.neighborTiles(currentNode.getX(), currentNode.getY(), false);
-	//
-	//			for (int[] neighbor : neighbors) {
-	//				//				System.out.println("X: " + neighbor[0] + " Y: " + neighbor[1]);
-	//				//				System.out.println("Movement: " + calculateMovementCost(neighbor[0], neighbor[1]));
-	//				Node newNode = new Node(neighbor[0], neighbor[1], calculateDistanceToGoal(neighbor[0], neighbor[1], goalX, goalY), currentNode.getMovementCost() + 1 + calculateMovementCost(neighbor[0], neighbor[1]), currentNode);
-	//
-	//				//				if (calculateMovementCost(neighbor[0],  neighbor[1]) > 990) {
-	//				//					newNode.setParent(null);
-	//				//					closedList.add(newNode);
-	//				//					continue;
-	//				//				}
-	//
-	//				boolean existsInOpenList = false;
-	//
-	//				for (Node n : openList) {
-	//
-	//					if (n.getX() == newNode.getX() && n.getY() == newNode.getY()) {
-	//						if (n.getMovementCost() < newNode.getMovementCost()) {
-	//							existsInOpenList = true; 
-	//							break;
-	//						}
-	//						else {
-	//							n.setParent(newNode.getParent());
-	//							n.setMovementCost(newNode.getMovementCost());
-	//							//System.out.println("Total cost (before): " + n.getTotalCost());
-	//							n.recalculateTotalCost();
-	//							//System.out.println("Total cost (after): " + n.getTotalCost());
-	//							existsInOpenList = true;
-	//							break;
-	//						}
-	//					}
-	//				}
-	//
-	//				if (!existsInOpenList) {  //node doesnt exist in the openList yet.. lets add it.
-	//					//	if (newNode.getTotalCost() < 100) 
-	//					openList.add(newNode); 
-	//				} else 
-	//				{/*do nothing*/};
-	//			}
-	//			if (currentNode.getTotalCost() < 100) { 
-	//				closedList.add(currentNode);
-	//			}
-	//
-	//		} while (openList.size() > 0);
-	//
-	//
-	//		if (openList.size() == 0 && goalFound == false) {
-	//			return null;  //TBI, what to return if goal not found...? Will this ever happen?
-	//		}
-	//
-	//		return tracePath(closedList.size() - 1); //start to trace from the back, where the goal-node can be found...
-	//
-	//	}
-
-
-
-	//	public static Stack<Node> calculatePath(int startX, int startY, int goalX, int goalY) {
-	//		//	assert(startX <= world.length || goalX <= world.length || startY <= world[0].length || goalY <= world[0].length);
-	//
-	//		/*if (startX == goalX && startY == goalY) {
-	//			return null; //TBI
-	//		}*/
-	//		boolean goalFound = false;
-	//
-	//		Node start = new Node(startX, startY, calculateDistanceToGoal(startX, startY, goalX, goalY), 0, null);
-	//		openList.add(start);		
-	//
-	//		do {
-	//
-	//			Node currentNode = findLowestTotalCost(openList);
-	//
-	//			openList.remove(currentNode);
-	//			closedList.add(currentNode);
-	//
-	//			if (currentNode.getX() == goalX && currentNode.getY() == goalY) { //Goal found
-	//				goalFound = true;
-	//				System.out.println("ClosedList: " + closedList.size());
-	//				System.out.println("GOAL FOUND");
-	//				break;
-	//			}
-	//
-	//			ArrayList<int[]> neighbors = HexagonUtils.neighborTiles(currentNode.getX(), currentNode.getY(), false);
-	//
-	//			for (int[] neighbor : neighbors) {
-	//				//if ( findSpecies(neighbor[0], neighbor[1]) ) {
-	//					//if water what to do?
-	//					Node newNode = new Node(neighbor[0], neighbor[1], calculateDistanceToGoal(neighbor[0], neighbor[1] , goalX, goalY), currentNode.getMovementCost() + calculateMovementCost(neighbor[0], neighbor[1]), currentNode);
-	//
-	//			
-	//					
-	//					
-	//					for (Node n : openList) {
-	//						
-	//						if (n.getX() == newNode.getX() && n.getY() == newNode.getY()) {
-	//							//node exists in openList, ev. update parent and calculate new movement cost and total cost
-	//							System.out.println("CurrentNode movementcost: " + currentNode.getMovementCost());
-	//							System.out.println("newNode movementcost: " + newNode.getMovementCost());
-	//							if ((currentNode.getMovementCost() + 1) < newNode.getMovementCost()) {
-	//								//TODO calculate new MovementCost and TotalValue
-	//								newNode.setParent(currentNode);
-	//								break;
-	//							}
-	//						}
-	//						
-	//					}
-	//					openList.add(newNode);
-	//
-	//
-	//
-	//				/*} else { //belongs to if (findSpecies...)
-	//					//species exists on neighbor[0], neighbor[1]. What to do? TBI
-	//				}*/
-	//			}
-	//
-	//		} while (openList.size() > 0);
-	//
-	//		if (openList.size() == 0 && goalFound == false) { return null; }
-	//
-	//		return tracePath(closedList);
-	//	}
-
-
 }
--- src/utilities/AstarDriver.java
+++ src/utilities/AstarDriver.java
@@ -1,6 +1,6 @@
 package utilities;
 
-import java.util.Stack;
+import java.util.Deque;
 
 public class AstarDriver {
 
@@ -8,55 +8,19 @@ public class AstarDriver {
 	public static int height = 6;
 	final static float WALL = 999.9f;
 
-	/*	static float[][] world ={ {0.0f,WALL,0.0f,0.0f},
-						      {0.0f,WALL,0.0f,0.0f}, 
-						      {0.0f,WALL,0.0f,0.0f},
-						      {0.0f,0.0f,  0.0f,0.0f} }; */
-
-	static float[][] world ={ {WALL,	WALL,	WALL,	WALL, 	WALL	, WALL},
-						  	  {WALL,	0.0f,  	0.0f,	WALL,	0.0f	, WALL}, 
-						  	  {WALL,	0.0f,  	WALL,	WALL,	0.0f	, WALL},
-						  	  {WALL,	0.0f,  	0.0f,  	WALL, 	0.0f	, WALL},
-						  	  {WALL,	0.0f,  	0.0f,  	0.0f, 	0.0f	, WALL},
-						  	  {WALL,	WALL,	WALL,	WALL, 	WALL	, WALL} };
+	static float[][] world ={ {WALL,	WALL,	WALL,	WALL, 	WALL, 	WALL},
+						  	  {WALL,	0.0f,  	0.0f,	WALL,	0.0f, 	WALL}, 
+						  	  {WALL,	0.0f,  	WALL,	WALL,	0.0f, 	WALL},
+						  	  {WALL,	0.0f,  	0.0f,  	WALL, 	0.0f, 	WALL},
+						  	  {WALL,	0.0f,  	0.0f,  	0.0f, 	0.0f, 	WALL},
+						  	  {WALL,	WALL,	WALL,	WALL, 	WALL, 	WALL} };
 	 
-	/*
-	static float[][] world ={ {10.0f,	0.0f,	0.0f,	0.0f, 	0.0f, 	0.0f},
-							  {20.0f,	0.0f,  	0.0f,	0.0f,	0.0f, 	0.0f}, 
-							  {30.0f,	0.0f,  	0.0f,	021.0f,	0.0f, 	04.0f},
-							  {40.0f,	0.0f,  	0.0f,  	0.0f, 	0.0f, 	0.0f},
-							  {50.0f,	0.0f,	0.0f,	0.0f, 	0.0f, 	0.0f} };
-	*/
-
 	public static void main(String[] args) {
-
-		//System.out.println(world.length);
-		//System.out.println(world[0].length);
-
-		/*	for (int i=0; i<4; i++) {
-			for (int j=0; j<4; j++) {
-				System.out.println(world[i][j]);
-			}
-		}*/
-//		System.out.println("----");
-//		for (int y = 0; y < world.length; y++) {
-//			for (int x = 0; x < world[0].length; x++) {
-//				System.out.println("(" + x + "," + y + "): " + world[y][x]);
-//			}
-//		}
-
-
-		Stack<Node> path = Astar.calculatePath(1, 1, 1, 4);
-
-//		System.out.println("Path size: " + path.size());
-
-//		for(Node n : path) {
-//
-//			System.out.println("X: " + n.getX() + ", Y: " + n.getY());
-//		}
-
-
-
+		Deque<Node> path = Astar.calculatePath(1, 1, 1, 4);
+		
+		for (Node node : path) {
+			System.out.println("(" + node.getX() + "," + node.getY() + ")");
+		}
 	}
 
 }
