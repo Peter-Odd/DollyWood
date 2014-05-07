7f613be643531454bc335fa67293a20e6a1796a2
Samuel Svensäter <peterodd@gmail.com>
2014-05-07 14:46:40 +0200

    Added new implementation

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -63,12 +63,6 @@ public class Main {
 		dayNightThread.start();
 
 		/* Test environment for tree */
-		/*Race treeRace = new Race("Tree");
-		Globals.races.add(treeRace);
-		Tree tree = new Tree(10, 10, treeRace);
-		treeRace.setSpeciesAt(10, 10, tree);
-		Thread treeThread = new Thread(tree);
-		treeThread.start();*/
 
 		float[][] treeArray = new float[Globals.height][Globals.width];
 		Random random;
@@ -79,21 +73,17 @@ public class Main {
 				treeArray[x][y] = random.nextFloat();
 			}
 		}
-		
-		Tree tree = new Tree(1000);	
-		Race treeRace = new Race("Tree");
 
+		Race treeRace = new Race("Tree");
+		Globals.races.add(treeRace);
+		
 		for (int x = 0; x < Globals.height; x++) {
 			for  (int y = 0; y < Globals.width; y++) {
-
 				if (treeArray[x][y] > 0.99) {
-					//if (treeRace.getSpeciesAt(x, y) == null) { //if water
-
-					Globals.races.add(treeRace);
+					Tree tree = new Tree(0, x, y);	
 					treeRace.setSpeciesAt(x, y, tree);
 					Thread treeThread = new Thread(tree);
 					treeThread.start();
-					//}
 				}
 
 
--- src/simulation/Tree.java
+++ src/simulation/Tree.java
@@ -3,41 +3,48 @@ package simulation;
 
 import utilities.Globals;
 import utilities.HexagonUtils;
+import utilities.Needs;
+import utilities.NeedsController;
+import utilities.NeedsController.NeedsControlled;
 
 public class Tree extends Animal implements Runnable {
 
 	private int tickLength;
-	private float[][] treeHealth;
+	private float treeHealth;
+	private int xPos;
+	private int yPos;
 
 
-	public Tree(int tickLength) {
+	public Tree(int tickLength, int xPos, int yPos) {
 		this.tickLength = tickLength;
-		treeHealth = new float[Globals.width][Globals.height];
+		treeHealth = 1.0f; //initial health value of 1.0f (max). ev random
+		this.xPos = xPos;
+		this.yPos = yPos;
 	}
-	
-	public float getTreeHealth(int x, int y) {
-		return treeHealth[x][y];
+
+	public float getTreeHealth() {
+		return treeHealth;
 	}	
-	/*En kommentar*/
+
 	private void step() {
-		for (int x = 0; x < Globals.width; x++) {
-			for (int y = 0; y < Globals.height; y++) {
-			//	float waterAmmount = 0.0f;
-				for (int[] neighbor : HexagonUtils.neighborTiles(x, y, true)) {
-				//	waterAmmount += Globals.water.getGroundWaterLevel(neighbor[0], neighbor[1]);
-					Globals.water.drawGroundWaterLevel(neighbor[0], neighbor[1], 1.0f);
-				}
-//				float treeGrowth = (float) (-(Math.pow(waterAmmount-3.5f, 2))/6.125f+1.0f);
-//				treeHealth[x][y] += treeGrowth*0.01f;
-//				
-//				if (treeHealth[x][y] > 1.0f)
-//					treeHealth[x][y] = 1.0f;
-//				else if (treeHealth[x][y] < 0.0f) 
-//					treeHealth[x][y] = 0.0f;
+		float waterAmmount = 0.0f;
+		for (int[] neighbor : HexagonUtils.neighborTiles(xPos, yPos, true)) {
+			//	waterAmmount += Globals.water.getGroundWaterLevel(neighbor[0], neighbor[1]);
+			//	Globals.water.drawGroundWaterLevel(neighbor[0], neighbor[1], 1.0f);
+			for (NeedsControlled nc : NeedsController.getNeed("Water")) {
+				waterAmmount += nc.getNeed(new Needs("Water", 1.0f), neighbor[0], neighbor[1]);
 			}
 		}
+		float treeGrowth = (float) (-(Math.pow(waterAmmount-3.5f, 2))/6.125f+1.0f);
+		treeHealth += treeGrowth*0.01f;
+
+		if (treeHealth > 1.0f)
+			treeHealth = 1.0f;
+		else if (treeHealth < 0.0f) 
+			treeHealth = 0.0f;
 	}
-	
+
+
 
 	public void run() {
 		while(true) {
