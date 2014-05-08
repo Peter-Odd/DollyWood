f8140f91e0946480233fd75d38bf2a6dda326a55
munHunger <marcusmunger@hotmail.com>
2014-05-08 23:39:48 +0200

    First draft of a sound system implemented

---
+++ src/utilities/SoundController.java
@@ -0,0 +1,60 @@
+package utilities;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import graphics.utilities.Camera;
+/**
+ * Sound controller.
+ * This class will make sure that sounds can be played without any hassle.
+ * @author OSM Group 5 - DollyWood project
+ * @version 1.0
+ */
+public class SoundController {
+	public static Camera camera;
+	public static float size;
+	
+	/**
+	 * Plays a sound with volume relative to the camera position.
+	 * Note that this is not in stereo. Camera rotation does not affect levels
+	 * @param sound A string with the name of the sound to be played
+	 * @param x position in the world where the sound should come from
+	 * @param y position in the world where the sound should come from
+	 */
+	public static void playSound(final String sound, final int x, final int y){
+		if(camera != null){
+			new Thread(new Runnable() {
+				public void run(){
+					try {
+						int[] cameraPos = camera.getArrayPosition(size);
+						int dist = Math.abs(x-cameraPos[0]) + Math.abs(y-cameraPos[1]);
+						
+						Clip clip = AudioSystem.getClip();
+						AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("res/" + sound));
+						clip.open(inputStream);
+						
+						FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+						gainControl.setValue(dist/100.0f);
+						clip.loop(0);
+					} catch (LineUnavailableException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (UnsupportedAudioFileException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}).start();
+		}
+	}
+}
