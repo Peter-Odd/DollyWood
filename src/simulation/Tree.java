91d21036e51887e66102918e9ef033b67372bca9
Samuel Svensäter <peterodd@gmail.com>
2014-05-12 14:42:36 +0200

    Tree is now spawning trees. Trees degrows sligthly when in total water and dies (disappears) if they have wilted 3 or more times. Still TODO: would be nicer if the trees continued to have negative growth as long as they are in total water (lake).

--- src/simulation/Tree.java
+++ src/simulation/Tree.java
@@ -1,6 +1,7 @@
 package simulation;
 
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import utilities.HexagonUtils;
@@ -15,31 +16,35 @@ import utilities.NeedsController.NeedsControlled;
  */
 public class Tree extends Animal implements Runnable {
 
-	private int tickLength;
-	private float treeHealth;
-	private int xPos;
-	private int yPos;
-	private Race race;
-	//	private Random random;
+	private int tickLength;		//ms between each step (loop)
+	private float treeHealth;	//a trees health
+	private float treeSeed;		//a trees seed amount
+	private int xPos;			//a trees x-position
+	private int yPos;			//a trees y-position
+	private int count;			//number of times a tree has "died"
+	private Race race;			//the race a tree belongs to
 
 	/**
 	 * 
 	 * @param tickLength Time (in ms) thread should sleep between "work"
 	 * @param xPos a trees x-coordinate
 	 * @param yPos a trees y-coordinate
-	 * @param race the race a tree belongs to
+	 * @param race a tree belongs to race
 	 */
 	public Tree(int tickLength, int xPos, int yPos, Race race) {
 		this.tickLength = tickLength;
-		treeHealth = new Random().nextFloat(); //initial health value of 1.0f (max). ev random
-		this.xPos = xPos;
-		this.yPos = yPos;
-		this.race = race;
-		setRotation(new Random().nextFloat() * 360); //Rotate tree randomized between 0 & 360 degrees
+		treeHealth = new Random().nextFloat(); 				//initial health value of random between 0.0f and 0.99..f
+		this.xPos = xPos;									//a trees x position
+		this.yPos = yPos;									//a trees y position
+		this.treeSeed = new Random().nextFloat(); 			//created random number for tree seed
+		this.treeSeed = (float) ((treeSeed * 0.5f) + 0.1f);	//random value for tree seed between 0.1f and 0.6f
+		this.race = race;									//the race a tree belongs to
+		setRotation(new Random().nextFloat() * 360); 		//Rotate tree randomized between 0 & 360 degrees, just to create some variation
+		this.count++;
 	}
 
 	public float getTreeHealth() {
-		return treeHealth; 
+		return treeHealth;
 	}	
 
 
@@ -48,9 +53,30 @@ public class Tree extends Animal implements Runnable {
 	}
 
 	/**
-	 * Each "tickLength" (see doc for constructor) this method will run once
+	 * Spawns a new tree if treeSeed is greater than 0.99. This value is increased once in a while
+	 */
+	private void spawnTree() {
+		if (treeSeed > 0.99 && treeHealth >= 1.0f) {	//lets spawn a tree
+			ArrayList<int[]> neighbors = HexagonUtils.neighborTiles(this.xPos, this.yPos, 1, false);
+			int randomSpawnedTree = new Random().nextInt(neighbors.size());
+			int treeXPos = neighbors.get(randomSpawnedTree)[0];
+			int treeYPos = neighbors.get(randomSpawnedTree)[1];
+			Tree tree = new Tree(400, treeXPos, treeYPos, this.race);
+			this.race.setSpeciesAt(treeXPos, treeYPos, tree);
+			Thread treeThread = new Thread(tree);
+			treeThread.start();	
+			this.treeSeed = 0.1f; //reset treeSeed when a tree have spawned a new tree
+		} else {
+			treeSeed += 0.01; //increase the treeSeed
+		}
+	}
+
+	/**
+	 * For each "tickLength" (see doc for constructor) this method will run once
 	 */
 	private void step() {
+		spawnTree(); 		//check if a new tree is to be spawned
+
 		float waterAmmount = 0.0f;
 
 		for (int[] neighbor : HexagonUtils.neighborTiles(xPos, yPos, true)) {
@@ -59,30 +85,34 @@ public class Tree extends Animal implements Runnable {
 			}
 		}
 		float treeGrowth = (float) (-(Math.pow(waterAmmount-3.5f, 2))/6.125f+1.0f);
-		//TODO: if too much water around tree, decrease treeHealth
+		
+		if (waterAmmount > 3.0f && treeHealth >= 1.45f) { //TODO: How to decrease health when tree in excess water?
+			treeHealth -= 0.5f; 	//decrease treeHealth
+			count++;
+		} else {
+			treeHealth += treeGrowth*0.02f; //increase treeHealth
+		}
 
-//		if (treeGrowth > 0.0f) {	//if trees should depend on sunlight
-//			float sunIntensity = 0.0f;
-//			for (NeedsControlled nc : NeedsController.getNeed("SunLight")) {
-//				sunIntensity += nc.getNeed(new Needs("SunLight", 0.4f), xPos, yPos);
-//			}
-//			treeHealth += sunIntensity;
-//		}
-
-		treeHealth += treeGrowth*0.02f; //increase treeHealth
+		//		if (treeGrowth > 0.0f) {	//if trees should depend on sunlight
+		//			float sunIntensity = 0.0f;
+		//			for (NeedsControlled nc : NeedsController.getNeed("SunLight")) {
+		//				sunIntensity += nc.getNeed(new Needs("SunLight", 0.4f), xPos, yPos);
+		//			}
+		//			treeHealth += sunIntensity;
+		//		}
 
 		if (treeHealth > 1.5f)
 			treeHealth = 1.5f;
-		else if (treeHealth < 0.0f) 
+		else if (treeHealth < 0.0f)  {
 			treeHealth = 0.0f;
-
-		treeHealth -= 0.01; //decrease treeHealth
-
-		//		if (treeHealth < 0.1) { //tree's health too bad, lets kill it
-		//			race.getAndRemoveSpeciesAt(xPos, yPos);
-		//		}
-
-		getSize(); //this manages the growth/degrowth of the tree
+			count++; 				//increase count (number of times a tree has "died"
+		}
+		
+		if (count >= 3) {			// a tree has "died" 3 times, let's remove it
+			race.getAndRemoveSpeciesAt(xPos, yPos);
+		} else {
+			treeHealth -= 0.01; 	//decrease treeHealth
+		}
 	} 
 
 
