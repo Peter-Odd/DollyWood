ad211a3aec2ca4847598cb55a82bda1e7637a558
Jimmy <jimmy.helmersson.6800@student.uu.se>
2014-05-13 12:25:48 +0200

    Sheeps now have gender, and female sheep can get pregnant. It's not working perfect, yet.

--- src/simulation/Animal.java
+++ src/simulation/Animal.java
@@ -5,15 +5,49 @@ import java.util.Random;
 public class Animal{
 	private Random random;
 	private float rotation;
+	private boolean gender; // true = female, false = male
+	private float age; // 1.0f = adult;
+	boolean pregnant;
 
-	
+		
 	public Animal(){
 		random = new Random();
 		rotation = random.nextInt(360);
+		age = 0.3f;
+		this.pregnant = false;
+		if(random.nextInt(100) >= 50){
+			this.gender = true;
+		} else {
+			this.gender = false;
+		}
+	}
+	
+	public Animal(boolean gender){
+		random = new Random();
+		rotation = random.nextInt(360);
+		age = 0.3f;
+		this.pregnant = false;
+		this.gender = gender;
+	}
+	
+	public boolean getGender(){
+		return gender;
+	}
+	
+	public float getAge(){
+		return age;
 	}
 	
 	public float getRotation() {
 		return rotation;
+	}
+	
+	public void setPregnant(boolean bool){
+		this.pregnant = bool;
+	}
+	
+	public boolean getPregnant(){
+		return pregnant;
 	}
 	
 	public void setRotation(float rotation){
--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -25,26 +25,40 @@ public class Main {
 
 
 
-		Globals.heightmap = Fractal.generateFractal(Globals.heightmap, 200.0f, 0.0f, 30.0f, 2.0f);
+		Globals.heightmap = Fractal.generateFractal(Globals.heightmap, 200.0f, 0.0f, 20.0f, 2.0f);
 
 
 		Race sheepRace = new Race("Sheep");
 		Globals.races.add(sheepRace);
-		Sheep sheep = new Sheep(15, 15, sheepRace);
+		Sheep sheep = new Sheep(15, 15, sheepRace, true);
 		sheepRace.setSpeciesAt(15, 15, sheep);
 		Thread sheepThread = new Thread(sheep);
 		sheepThread.start();
 		
-		Sheep sheep1 = new Sheep(16, 20, sheepRace);
+		Sheep sheep1 = new Sheep(16, 20, sheepRace, false);
 		sheepRace.setSpeciesAt(16, 20, sheep1);
 		Thread sheepThread1 = new Thread(sheep1);
 		sheepThread1.start();
 		
-		Sheep sheep2 = new Sheep(17, 10, sheepRace);
+		/*Sheep sheep2 = new Sheep(17, 10, sheepRace);
 		sheepRace.setSpeciesAt(17, 10, sheep2);
 		Thread sheepThread2 = new Thread(sheep2);
 		sheepThread2.start();
 		
+		Sheep sheep3 = new Sheep(20, 10, sheepRace);
+		sheepRace.setSpeciesAt(20, 10, sheep3);
+		Thread sheepThread3 = new Thread(sheep3);
+		sheepThread3.start();
+		
+		Sheep sheep4 = new Sheep(18, 7, sheepRace);
+		sheepRace.setSpeciesAt(18, 7, sheep4);
+		Thread sheepThread4 = new Thread(sheep4);
+		sheepThread4.start();
+		
+		Sheep sheep5 = new Sheep(17, 23, sheepRace);
+		sheepRace.setSpeciesAt(17, 23, sheep5);
+		Thread sheepThread5 = new Thread(sheep5);
+		sheepThread5.start();*/
 		
 		Globals.water = new Water(100);
 		Thread waterThread = new Thread(Globals.water);
@@ -58,7 +72,6 @@ public class Main {
 		dayNightThread.start();
 
 		new Graphics3D();
-		//new Graphics2D();
 	}
 
 
