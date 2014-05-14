5018d932bf0d3a85d90d922279bab91f2aeb5e35
Jimmy <jimmy.helmersson.6800@student.uu.se>
2014-05-14 16:30:44 +0200

    Added an synchronization array, which now is used by the methods in race.

--- src/simulation/Race.java
+++ src/simulation/Race.java
@@ -1,5 +1,7 @@
 package simulation;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import utilities.Globals;
 import utilities.HexagonUtils;
 import utilities.Needs;
@@ -8,11 +10,20 @@ import utilities.NeedsController.NeedsControlled;
 
 public class Race implements NeedsControlled{
 	private Animal[][] species;
+	private AtomicInteger[][] lockArray;
 	private String specName;
 
 	public Race(String specName) {
 		this.specName = specName;
 		species = new Animal[Globals.height][Globals.width];
+		lockArray = new AtomicInteger[Globals.height][Globals.width];
+		
+		//initates the lockArray.
+		for(int x = 0; x<Globals.width;x++){
+			for(int y = 0; y<Globals.height;y++){
+				lockArray[x][y] = new AtomicInteger(1);
+			}
+		}
 
 		if(specName == "Sheep"){
 			NeedsController.registerNeed("Meat", this);
@@ -28,40 +39,101 @@ public class Race implements NeedsControlled{
 	public Animal getSpeciesAt(int x, int y) {
 		return species[x][y];
 	}
+	
+	/** Removes and returns the animal at the coordinates (x, y), if the cell is unlocked.
+	 * 
+	 * @param x Coordinate to get from.
+	 * @param y Coordinate to get from.
+	 * @return An animal if the cell is unlocked, else it returns null.
+	 */
 
 	public Animal getAndRemoveSpeciesAt(int x, int y){
-		Animal animal = species[x][y];
-		species[x][y] = null;
-		return animal;
+		if(lockArray[x][y].compareAndSet(1, 0)){
+			Animal animal = species[x][y];
+			species[x][y] = null;
+			lockArray[x][y].incrementAndGet();
+			return animal;
+		}
+		return null;
 	}
+	
+	/** Moves an animal from (currentX, currentY) to (newX, newY), if that cell is unlocked and there is nothing else
+	 *  on that cell.
+	 * 
+	 * @param currentX Current x coordinate.
+	 * @param currentY Current y coordinate.
+	 * @param newX Coordinate which the object want to move to.
+	 * @param newY Coordinate which the object want to move to.
+	 * @return True if successfully moved, else false.
+	 */
 
 	public boolean moveSpecies(int currentX, int currentY, int newX, int newY){
-
-		if(species[newX][newY] == null){
-			Animal animal = getAndRemoveSpeciesAt(currentX, currentY);
-			species[newX][newY] = animal;
+		
+		if(newX == currentX && newY == currentY){
 			return true;
-		} else {
-			return false;
 		}
+		
+		if(lockArray[newX][newY].compareAndSet(1, 0)){
+			if(species[newX][newY] == null){
+				Animal animal = getAndRemoveSpeciesAt(currentX, currentY);
+				if(animal == null){
+					return false;
+				}else{
+					species[newX][newY] = animal;
+					animal.calcRotation(newX, newY);
+					animal.setXandYPos(newX, newY);
+					lockArray[newX][newY].incrementAndGet();
+					return true;
+				}
+			} else {
+				lockArray[newX][newY].incrementAndGet();
+				return false;
+			}
+		}
+		return false;
 	}
 
-	public boolean setSpeciesAt(int x, int y, Animal s) {
-		if (species[x][y] == null) {
-			species[x][y] = s;
-			return true;
-		} 
-		else {
-			return false;
+	/** Sets animal at coordinates (x, y) if the cell is unlocked and is not occupied, else it does nothing.
+	 * 
+	 * @param x The x coordinate which the animal want to be spawned.
+	 * @param y The y coordinate which the animal want to be spawned.
+	 * @param animal The animal which will be spawned.
+	 * @return True if successfully spawned, else false.
+	 */
+	
+	public boolean setSpeciesAt(int x, int y, Animal animal) {
+		if(lockArray[x][y].compareAndSet(1, 0)){
+			if (species[x][y] == null) {
+				species[x][y] = animal;
+				lockArray[x][y].incrementAndGet();
+				return true;
+			} 
+			else {
+				lockArray[x][y].incrementAndGet();
+				return false;
+			}
 		}
+		return false;
 	}
+	
+	/** Returns true if the cell at coordinates (x, y) is unlocked and contains an animal.
+	 * 
+	 * @param x The coordinate to be checked.
+	 * @param y The coordinate to be checked.
+	 * @return True if the cell at coordinates (x, y) is unlocked and contains an animal.
+	 */
 
 	public boolean containsAnimal(int x, int y){
-		if(species[x][y] != null){
-			return true;
-		} else {
-			return false;
+		if(lockArray[x][y].compareAndSet(1, 0)){
+			if(species[x][y] != null){
+				lockArray[x][y].incrementAndGet();
+				return true;
+			} else {
+				lockArray[x][y].incrementAndGet();
+				return false;
+			}
 		}
+		return false;
 	}
 
 
