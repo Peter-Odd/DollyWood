ba2b92f7fa07b6f918c362b9e783484740c57407
Samuel Svensäter <peterodd@gmail.com>
2014-05-21 13:57:23 +0200

    Changed initial sheep and wolf count.

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -26,12 +26,12 @@ public class Main {
 		Globals.registerSetting("Tree seed amount", "Tree", 0.0f, 1.0f, 0.3f);
 		Globals.registerSetting("Tree time of life (sec)", "Tree", 0, 500, 200);
 		
-		Globals.registerSetting("Number of sheep", "Sheep", 2, 40, 5);
+		Globals.registerSetting("Number of sheep", "Sheep", 2, 250, 25);
 		Globals.registerSetting("Sheep sleep", "Sheep", 0, 1500, 500);
 		Globals.registerSetting("Sheep thirst", "Sheep", 0.1f, 1, 0.5f);
 		Globals.registerSetting("Sheep hunger", "Sheep", 0.1f, 1, 0.5f);
 		
-		Globals.registerSetting("Number of wolves", "Wolf", 2, 40, 5);
+		Globals.registerSetting("Number of wolves", "Wolf", 2, 250, 15);
 		Globals.registerSetting("Wolves sleep", "Wolf", 0, 1500, 500);
 		Globals.registerSetting("Wolves thirst", "Wolf", 0.1f, 1, 0.5f);
 		Globals.registerSetting("Wolves hunger", "Wolf", 0.1f, 1, 1.0f);
